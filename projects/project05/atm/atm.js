5852cd77d834d1301db05c34aa8a8d40bc1faf06
jay ess <mentoce@gmail.com>
2020-04-21 22:17:39 -0700

    Finish ATM app

--- projects/project05/atm/atm.js
+++ projects/project05/atm/atm.js
@@ -11,17 +11,93 @@ Project 5 - ATM Program
 3. Output results using innerHTML
 4. ​Make sure to validate for overdraft
 */
+// add two event listeners on window load
+window.onload =  function() {
+    document.getElementById('btnDeposit').addEventListener('click', deposit);
+    document.getElementById('btnWithdraw').addEventListener('click', withdraw);
+}
 
+// return the current balance as a number
 function balanceCheck() {
-
+    return Number(document.getElementById('balance').innerText);
 }
 
+// withdraw money from the account if possible
 function withdraw() {
+    // clear any errors that may be left over from previous transactions
+    clearErrors();
+    var withdrawalAmount = valueById('withdrawalAmount');
 
+    // make sure that the withdrawal won't result in an
+    // overdraft and if it does display an error to the user
+    if (withdrawalAmount < 0) {
+        // update the balance with the negative of the
+        // withdrawal amount
+        negativeAmountError();
+    } else if (!overdraft(withdrawalAmount)) {
+        updateBalance(-withdrawalAmount);
+    } else {
+        overdraftError();
+    }
 }
 
+// deposit money into the account if possible
 function deposit() {
+    // clear any errors that may be left over from previous transactions
+    clearErrors();
+    var amount = valueById('depositAmount');
 
+    // check that the amount is positive, if not display an error to the user
+    if (amount >= 0) {
+        updateBalance(amount);
+    } else {
+        negativeAmountError();
+    }
+}
+
+// update the balance
+function updateBalance(amount) {
+    var newBalance = balanceCheck() + amount;
+    addText('balance', numAsCurrency(newBalance));
+}
+
+// check if a withdrawal will result in an overdraft (negative balance)
+function overdraft(withdrawalAmount) {
+    return balanceCheck() - withdrawalAmount < 0;
+}
+
+// display an error message to the user about an overdraft
+function overdraftError() {
+    var msg = '<p id="errors">ERROR: overdraft. Can\'t withdraw that amount.</p>';
+    setErrorMsg(msg);
+}
+
+// clear the errors
+function clearErrors() {
+    addText('errors', '');
+}
+
+// display an error to the user stating about an attempt to deposit/withdraw
+// a negative amount
+function negativeAmountError() {
+    var msg = '<p id="errors">ERROR: negative amount. ';
+    msg += "Can't deposit/withdraw a negative number.</p>";
+    setErrorMsg(msg);
+}
+
+// sets the passed in error message
+function setErrorMsg(msg) {
+    addText('errors', msg);
+}
+
+// Adds text to the given element
+function addText(elem, html) {
+    document.getElementById(elem).innerHTML = html;
+}
+
+// returns the value at the given element id
+function valueById(elemId) {
+    return Number(document.getElementById(elemId).value);
 }
 
 // Converts and returns a number to a currency string. E.g.,
@@ -39,7 +115,7 @@ function numAsCurrency(num) {
     var cents = Math.round((num - dollars) * 100);
 
     // Create the currency string: $dollars.cents
-    var currency = "$" + dollars + ".";
+    var currency = dollars + ".";
 
     if (cents < 10) {
         // If there are less than 10 cents, then we need to concatenate a
