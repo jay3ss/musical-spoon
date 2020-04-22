d12f8600fa8f290a30e82ebf3136ffbde6d42a48
jay ess <mentoce@gmail.com>
2020-04-21 20:09:08 -0700

    Add dummy functions and num to currency conversion function

--- projects/project05/atm/atm.js
+++ projects/project05/atm/atm.js
@@ -11,5 +11,44 @@ Project 5 - ATM Program
 3. Output results using innerHTML
 4. ​Make sure to validate for overdraft
 */
-var name = prompt("Please input your first and last name: ");
-document.write("<p>Hello " + name + ", how is your day going?</p>");
+
+function balanceCheck() {
+
+}
+
+function withdraw() {
+
+}
+
+function deposit() {
+
+}
+
+// Converts and returns a number to a currency string. E.g.,
+// 123.4567 -> $123.46
+function numAsCurrency(num) {
+    // Extract the dollar and cents values
+    // Remove the dollar amount from the number by rounding down to the
+    // ones place
+    var dollars = Math.floor(num);
+
+    // Remove the cents amount from the number by:
+    // 1. Subtract the dollar amount
+    // 2. Multiply the result by 100
+    // 3. Round the result to the nearest ones place
+    var cents = Math.round((num - dollars) * 100);
+
+    // Create the currency string: $dollars.cents
+    var currency = "$" + dollars + ".";
+
+    if (cents < 10) {
+        // If there are less than 10 cents, then we need to concatenate a
+        // zero to the currency string BEFORE we concatenate the cents value
+        currency += "0" + cents;
+    } else {
+        // If there are more than 10 cents, then we can just concatenate
+        // the cents straight away
+        currency += cents;
+    }
+    return currency;
+}
