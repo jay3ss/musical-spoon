66f563b2070518e25123fb51fe7ec151ca555946
jay ess <mentoce@gmail.com>
2020-05-19 13:00:28 -0700

    Add shoplist object

--- projects/project07/shoplist/shoplist.js
+++ projects/project07/shoplist/shoplist.js
@@ -7,3 +7,25 @@ Create a folder called shoplist. In the html file create a form that takes
 shopping list items.  Use JavaScript to add the items in an unordered list on
 the HTML file
 */
+
+// Shoplist object to hold the list of items in the shopping list
+let shoplist = {
+    isInited: false,
+    items: [],
+    addItem: function (item) {
+        items.append(item);
+    },
+    init: function() {
+        isInited = true;
+        return isInited;
+    },
+    itemsToList: function () {
+        let itemsList = "<ul id='items'>";
+        for (let i = 0; i < items.length; i++) {
+            itemsList += "<li>" + items[0] + "</li>";
+        }
+        itemsList += "</ul>";
+
+        return itemsList;
+    }
+}
