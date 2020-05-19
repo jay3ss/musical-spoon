1f3ce67e35b0ccbbd08e9b7da12bd1745d85e108
jay ess <mentoce@gmail.com>
2020-05-19 11:42:18 -0700

    Add project info

---
+++ projects/project07/shoplist/shoplist.js
@@ -0,0 +1,9 @@
+/* file: shoplist.js
+Joshua Saunders
+CS 150
+Project 7 - Dynamic Shop List
+
+Create a folder called shoplist. In the html file create a form that takes
+shopping list items.  Use JavaScript to add the items in an unordered list on
+the HTML file
+*/
